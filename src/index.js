2a0b04f38c13f8b52674edcd1be16fce9c2b769c
Stephen Landis <stephen.n.landis@gmail.com>
2017-02-07 12:32:24 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import './styles/index.css';
+
+import Main from './Main';
+// import Apply from './Apply';
+// import Confirmed from './Confirmed';
+
+
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
+);
